1f85270b96f9805a01deaa5e550f3571e0fc5430
SabineWanka <sabinewanka@yahoo.de>
2020-03-10 19:06:23 +0100

    add icons to footer

--- src/views/AppFooter.js
+++ src/views/AppFooter.js
@@ -89,19 +89,13 @@ export default function AppFooter() {
             >
               <Grid item className={classes.icons}>
                 <a href="https://material-ui.com/" className={classes.icon}>
-                  <img
-                    src="/static/themes/onepirate/appFooterFacebook.png"
-                    alt="Facebook"
-                  />
+                  <img src="images/facebook.png" alt="Facebook" />
                 </a>
                 <a
                   href="https://twitter.com/MaterialUI"
                   className={classes.icon}
                 >
-                  <img
-                    src="/static/themes/onepirate/appFooterTwitter.png"
-                    alt="Twitter"
-                  />
+                  <img src="images/twitter.png" alt="Twitter" />
                 </a>
               </Grid>
               <Grid item>
